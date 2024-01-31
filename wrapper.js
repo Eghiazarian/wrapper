bdfde28e1fee71b6e62d1ce2cd9e19aedcee4496
Armen <eghiazarian.armen@gmail.com>
2024-01-31 13:13:47 +0400

    initial commit

---
+++ wrapper.js
@@ -0,0 +1,13 @@
+let wrap = document.getElementById('wrap')
+let divResult = document.getElementById('result')
+wrap.onclick = () => {
+
+    let text = document.getElementById('text').value
+    divResult.innerHTML = `<div>${strWrapper(text, "[", "]")}</div>`
+
+}
+
+function strWrapper(str, left = "«", right = "»") {
+    return str ? left + str + right : "null"
+
+}
