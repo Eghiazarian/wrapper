16691b1b34449e8a4a8abbb476f0f866336d579b
Armen <eghiazarian.armen@gmail.com>
2024-01-31 13:22:01 +0400

    cleared whitespaces

--- wrapper.js
+++ wrapper.js
@@ -1,13 +1,9 @@
 let wrap = document.getElementById('wrap')
 let divResult = document.getElementById('result')
 wrap.onclick = () => {
-
     let text = document.getElementById('text').value
     divResult.innerHTML = `<div>${strWrapper(text, "[", "]")}</div>`
-
 }
-
 function strWrapper(str, left = "«", right = "»") {
     return str ? left + str + right : "null"
-
 }
